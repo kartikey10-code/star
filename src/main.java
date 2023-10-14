7b1a242a952ca830711823205daab09958ebe526
kartikey <kartikeynarayan1998@outlook.com>
2023-10-15 01:02:56 +0530

    commit 2

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ public class main {
         for (int i = 1; i <= rows; ++i) {
           for (int j = 1; j <= i; ++j) {
             System.out.print("* ");
+            System.out.println("hello");
           }
           System.out.println();
         }
